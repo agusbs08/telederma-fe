531f860c8973ba745410876dc9c5b80fb9a2d4de
Syahrul Arifuddin <syahrulak98@gmail.com>
2019-07-26 20:58:33 +0700

    extract layout from templates

---
+++ resources/views/components/sidebar/puskesmas.blade.php
@@ -0,0 +1,19 @@
+<li class="app-sidebar__heading">Menu</li>
+<li>
+    <a href="charts-chartjs.html">
+        <i class="metismenu-icon pe-7s-graph2">
+        </i>Dashboard
+    </a>
+</li>
+<li>
+    <a href="charts-apexcharts.html">
+        <i class="metismenu-icon pe-7s-graph">
+        </i>Pasien
+    </a>
+</li>
+<li>
+    <a href="charts-sparklines.html">
+        <i class="metismenu-icon pe-7s-graph1">
+        </i>Pemeriksaan
+    </a>
+</li>

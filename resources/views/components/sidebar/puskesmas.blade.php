2455cda328b56061e109a239dd70d934c5f47a05
Syahrul Arifuddin <syahrulak98@gmail.com>
2019-07-27 19:22:02 +0700

    create patient details layout

--- resources/views/components/sidebar/puskesmas.blade.php
+++ resources/views/components/sidebar/puskesmas.blade.php
@@ -1,18 +1,22 @@
+@php
+$class = "class=\"mm-active\"";
+$pagename = Request::segment(2);
+@endphp
 <li class="app-sidebar__heading">Menu</li>
 <li>
-    <a href="charts-chartjs.html">
+    <a href="{{ route('puskesmas.dashboard') }}" @php echo $pagename=="dashboard" ? $class : null @endphp>
         <i class="metismenu-icon pe-7s-graph2">
         </i>Dashboard
     </a>
 </li>
 <li>
-    <a href="charts-apexcharts.html">
+    <a href="{{ route('puskesmas.patients') }}" @php echo $pagename=="patients" ? $class : null @endphp>
         <i class="metismenu-icon pe-7s-graph">
         </i>Pasien
     </a>
 </li>
 <li>
-    <a href="charts-sparklines.html">
+    <a href="{{ route('puskesmas.examinations') }}" @php echo $pagename=="examinations" ? $class : null @endphp>
         <i class="metismenu-icon pe-7s-graph1">
         </i>Pemeriksaan
     </a>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('master/base');
-});
+Route::get('/puskesmas/patients', function () {
+    return view('partials.puskesmas.patients-list');
+})->name('puskesmas.patients');
+
+Route::get('/puskesmas/patients/{patient_id}/details', function () {
+    return view('partials.puskesmas.patient-detail');
+})->name('puskesmas.patient-details');
+
+Route::get('/puskesmas/examinations', function () {
+    return view('partials.puskesmas.examinations-list');
+})->name('puskesmas.examinations');
+
+Route::get('/puskesmas/dashboard', function () {
+    return view('partials.puskesmas.patients-list');
+})->name('puskesmas.dashboard');
